2cbc9cd9b12061a96181aa05b47a5c09af5f9cda
Taylor Bisset <taylorbisset@gmail.com>
2023-05-16 11:55:47 -0700

    Comment out old code. Create fallthrough path. Create string values from int input.

--- CS110-MonthYearConverterWithSwitch/CS110-MonthYearConverterWithSwitch.cpp
+++ CS110-MonthYearConverterWithSwitch/CS110-MonthYearConverterWithSwitch.cpp
@@ -5,82 +5,97 @@ using namespace std;
 int main()
 {
 	int month = 1;
-	int yearToCheck = 2000;
-	
+	string monthString = "January";
+	int year = 2000;
+
 	cout << "Please input what month you are checking? ";
 	cin >> month;
 	cout << endl;
 
 	cout << "Please input what year you are checking? ";
-	cin >> yearToCheck;
+	cin >> year;
 	cout << endl;
+
+	if (month == 1) { monthString = "January"; }
+	if (month == 2) { monthString = "February"; }
+	if (month == 3) { monthString = "Marh"; }
+	if (month == 4) { monthString = "April"; }
+	if (month == 5) { monthString = "May"; }
+	if (month == 6) { monthString = "June"; }
+	if (month == 7) { monthString = "July"; }
+	if (month == 8) { monthString = "August"; }
+	if (month == 9) { monthString = "September"; }
+	if (month == 10) { monthString = "October"; }
+	if (month == 11) { monthString = "November"; }
+	if (month == 12) { monthString = "December"; }
 
 	switch (month)
 	{
+/*
 		case 1:
-			cout << "January " << yearToCheck << ", has 31 days.\n";
+			cout << "January " << year << ", has 31 days.\n";
 			break;
 
 		// February, checking for leap year.
 		case 2:
 
-			if (yearToCheck % 400 == 0.0)
+			if (year % 400 == 0.0)
 			{
-				cout << endl << "In the year " << yearToCheck << ", February has 29 days.\n";
+				cout << endl << "In the year " << year << ", February has 29 days.\n";
 			}
 
-			else if (yearToCheck % 4 == 0.0 and yearToCheck % 100 != 0.0)
+			else if (year % 4 == 0.0 and year % 100 != 0.0)
 			{
-				cout << endl << "In the year " << yearToCheck << ", February has 29 days.\n";
+				cout << endl << "In the year " << year << ", February has 29 days.\n";
 			}
 
 			else
 			{
-				cout << endl << "In the year " << yearToCheck << ", February has 28 days.\n";
+				cout << endl << "In the year " << year << ", February has 28 days.\n";
 			}
 			break;
 			// End leap year logic.
 			
 		case 3:
-			cout << "March " << yearToCheck << ", has 31 days.\n";
+			cout << "March " << year << ", has 31 days.\n";
 			break;
 
 		case 4:
-			cout << "April " << yearToCheck << ", has 30 days.\n";
+			cout << "April " << year << ", has 30 days.\n";
 			break;
 
 		case 5:
-			cout << "May " << yearToCheck << ", has 31 days.\n";
+			cout << "May " << year << ", has 31 days.\n";
 			break;
 
 		case 6:
-			cout << "June " << yearToCheck << ", has 30 days.\n";
+			cout << "June " << year << ", has 30 days.\n";
 			break;
 
 		case 7:
-			cout << "July " << yearToCheck << ", has 31 days.\n";
+			cout << "July " << year << ", has 31 days.\n";
 			break;
 
 		case 8:
-			cout << "August " << yearToCheck << ", has 31 days.\n";
+			cout << "August " << year << ", has 31 days.\n";
 			break;
 
 		case 9:
-			cout << "September " << yearToCheck << ", has 30 days.\n";
+			cout << "September " << year << ", has 30 days.\n";
 			break;
 
 		case 10:
-			cout << "October " << yearToCheck << ", has 31 days.\n";
+			cout << "October " << year << ", has 31 days.\n";
 			break;
 
 		case 11:
-			cout << "November " << yearToCheck << ", has 30 days.\n";
+			cout << "November " << year << ", has 30 days.\n";
 			break;
 
 		case 12:
-			cout << "December " << yearToCheck << ", has 31 days.\n";
+			cout << "December " << year << ", has 31 days.\n";
 			break;
-/*
+*/
 		case 1:
 		case 3:
 		case 5:
@@ -88,33 +103,33 @@ int main()
 		case 8:
 		case 10:
 		case 12:
-			cout << month << " " << yearToCheck << ", has 31 days.";
+			cout << monthString << " " << year << ", has 31 days.";
 			break;
 
 		case 4:
 		case 6:
 		case 9:
 		case 11:
-			cout << month << " " << yearToCheck << ", has 30 days.";
+			cout << monthString << " " << year << ", has 30 days.";
 			break;
 
 		case 2:
-			if (yearToCheck % 400 == 0.0)
+			if (year % 400 == 0.0)
 			{
-				cout << endl << "February " << yearToCheck << ", has 29 days.\n";
+				cout << endl << "February " << year << ", has 29 days.\n";
 			}
 
-			else if (yearToCheck % 4 == 0.0 and yearToCheck % 100 != 0.0)
+			else if (year % 4 == 0.0 and year % 100 != 0.0)
 			{
-				cout << endl << "February " << yearToCheck << ", has 29 days.\n";
+				cout << endl << "February " << year << ", has 29 days.\n";
 			}
 
 			else
 			{
-				cout << endl << "February " << yearToCheck << ", has 28 days.\n";
+				cout << endl << "February " << year << ", has 28 days.\n";
 			}
 			break;
-*/
+
 	}
 	cout << endl;
 }
