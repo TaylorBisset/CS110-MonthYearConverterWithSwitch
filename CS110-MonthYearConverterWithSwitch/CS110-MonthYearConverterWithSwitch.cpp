6c003bbfc4756327cda14e18a689f294e61d578c
Taylor Bisset <taylorbisset@gmail.com>
2023-05-16 10:51:51 -0700

    Created initial user prompts. Added January.

--- CS110-MonthYearConverterWithSwitch/CS110-MonthYearConverterWithSwitch.cpp
+++ CS110-MonthYearConverterWithSwitch/CS110-MonthYearConverterWithSwitch.cpp
@@ -4,5 +4,15 @@ using namespace std;
 
 int main()
 {
+	cout << "Please input what month you are checking? ";
+	int month = 1;
+	cin >> month;
+	cout << endl;
 
+	switch (month)
+	{
+	case 1:
+		printf("January has 31 days.\n");
+			break;
+	}
 }
