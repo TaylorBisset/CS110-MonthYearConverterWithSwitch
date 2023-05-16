b129a4f49a4000100a03e28cba2ae8232298ac88
Taylor Bisset <taylorbisset@gmail.com>
2023-05-16 10:58:30 -0700

    Added logic for February, checking if it is a leap year.

--- CS110-MonthYearConverterWithSwitch/CS110-MonthYearConverterWithSwitch.cpp
+++ CS110-MonthYearConverterWithSwitch/CS110-MonthYearConverterWithSwitch.cpp
@@ -14,5 +14,28 @@ int main()
 	case 1:
 		printf("January has 31 days.\n");
 			break;
+
+	case 2:
+		printf("What year are you checking? ");
+		int yearToCheck = 2000;
+		cin >> yearToCheck;
+
+		if (yearToCheck % 400 == 0.0)
+		{
+			cout << endl << "In the year " << yearToCheck << ", February has 29 days.\n";
+		}
+
+		else if (yearToCheck % 4 == 0.0 and yearToCheck % 100 != 0.0)
+		{
+			cout << endl << "In the year " << yearToCheck << ", February has 29 days.\n";
+		}
+
+		else
+		{
+			cout << endl << "In the year " << yearToCheck << ", February has 28 days.\n";
+		}
+		break;
+
 	}
+	cout << endl;
 }
